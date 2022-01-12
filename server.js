d8a49c5ab96a611e9c646e9a670a33c625c42e97
ksriram33 <ksriramora@gmail.com>
2022-01-12 05:43:15 +0000

    Firestone COmmit

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ const app = express();
 // the backend server will parse json, not a form request
 app.use(bodyParser.json());
 
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+
 // allow AJAX calls from 3rd party domains
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -44,13 +55,28 @@ app.get('/version', (req, res) => {
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    // res.json(mockEvents);
+    getEvents(req, res);
 });
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
-// this will produce unexpected behavior in a stateless kubernetes cluster. 
+// this will produce unexpected behavior in a stateless kubernetes cluster.
 app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    const ev = { 
+        title: req.body.title, 
+        description: req.body.description,
+        id : mockEvents.events.length + 1
+     }
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+});
+
+
+/*app.post('/event', (req, res) => {
     // create a new object from the json data and add an id
     const ev = { 
         title: req.body.title, 
@@ -61,7 +87,28 @@ app.post('/event', (req, res) => {
     mockEvents.events.push(ev);
     // return the complete array
     res.json(mockEvents);
-});
+});*/
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
